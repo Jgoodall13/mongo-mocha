58d4bcc1aab79936d0146d81c250eb5ce772788d
Jacob Goodall <jgoodall@opp.local>
2018-09-04 17:01:34 -0500

    promises

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 
-console.log('In main file')
-mongoose.connect('mongodb://localhost/users_test', { useNewUrlParser: true });
-mongoose.connection
-    .once('open', () => console.log('Good to go!'))
-    .on('error', (error) => {
-        console.log('WE IN THIS ERRRRRRRRRRRRRRRRRRRRRRROR')
-        console.warn("Warning", error)
+before((done) => {
+    mongoose.connect('mongodb://localhost/users_test', {
+        useNewUrlParser: true
     });
+    mongoose.connection
+        .once('open', () => { done(); })
+        .on('error', (error) => {
+            console.log('WE IN THIS ERRRRRRRRRRRRRRRRRRRRRRROR')
+            console.warn("Warning", error)
+        });
+})
+
 
 beforeEach((done) => {
     mongoose.connection.collections.users.drop(() => {
